8a6f000599853c5f0d95c678c9338d88f33a9e5b
Guildart <romainmussard974@gmail.com>
2020-06-13 19:42:48 +0200

    Bug Drop réglé

--- Assets/Controller/PawnController.cs
+++ Assets/Controller/PawnController.cs
@@ -1,23 +1,32 @@
-﻿using Tfi;
+﻿using System;
+using Tfi;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Controller
 {
-    public class PawnController: MonoBehaviour, IDragHandler, IEndDragHandler
+    public class PawnController: MonoBehaviour, IDragHandler, IEndDragHandler,IBeginDragHandler
     {
-        private Vector3 initPos;
-        private Player player;
+        public Vector3 initPos;
+        public Player player;
+        public static GameObject isBeingDragged; //Le prefab qui est dragg = 1 seul à la fois
+
         void Start()
         {
             initPos = transform.localPosition;
-            initPos = new Vector3(initPos.x, initPos.y, 1);
+            initPos = new Vector3(initPos.x, initPos.y, -1);
             //print(initPos);
         }
-        
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            isBeingDragged = gameObject;
+        }
+
         public void OnDrag(PointerEventData eventData)
         {
             Vector3 mousePos = Input.mousePosition;
+            transform.GetComponent<CanvasGroup>().blocksRaycasts = false;
         
             if (Camera.main != null)
             {
@@ -26,33 +35,17 @@ namespace Controller
 
                 transform.position = new Vector3(transform.position.x, transform.position.y);
             }
-            //print(transform.position);
+            print(transform.position);
 
         
         }
     
         public void OnEndDrag(PointerEventData eventData)
         {
-            Vector3 worldPosition1;
-            //Vector3 mousePos = Input.mousePosition;
-            transform.position = new Vector3(transform.position.x, transform.position.y, 1); // important pour detecter la zone
-
-            if (Camera.main != null)
-            {
-                worldPosition1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                worldPosition1.z = -1 * worldPosition1.z;
-                print(-1*Vector2.up);
-                print(Vector2.up);
-                RaycastHit2D hit = Physics2D.Raycast(worldPosition1,  Vector2.up);
-                if (hit.collider != null)
-                {
-                    print(hit.collider.name);
-                    print(hit.collider.GetComponent<ZoneController>().GetZone().getPosition().ToString()); // ici je recupere un composant de la zaone
-                    // il suffit alors de faire ce que tu as à faire...
-                }
-
-            }
-            transform.localPosition = initPos;
+            
+            transform.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            isBeingDragged = null;
+            //transform.localPosition = initPos;
         
         }
 
