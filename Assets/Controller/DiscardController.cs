459a5c652f3c9e3fc03e4851524fa8277b92aabe
Guildart <romainmussard974@gmail.com>
2020-06-18 05:02:06 +0200

    Bug au Build

--- Assets/Controller/DiscardController.cs
+++ Assets/Controller/DiscardController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Controller;
 using Tfi;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
